739b2e853dac62c8f441c2a527bb53d43adcbd33
Milo Weinberg <git@iapetus11.me>
2026-07-27 17:47:42 -0400

    Background blur

--- linux.lua
+++ linux.lua
@@ -5,6 +5,8 @@ local module = {}
 
 function module.apply_config(config)
 	config.font_size = 11
+	config.window_background_opacity = 0.1
+	config.wayland_window_background_blur = true
 	config.initial_cols = 90
 	config.initial_rows = 24
 end
