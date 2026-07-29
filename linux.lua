3c267f777332f700b8c0aa269d125649e9fdc9df
Milo Weinberg <git@iapetus11.me>
2026-07-29 00:18:00 -0400

    Disable broken wayland background blur

--- linux.lua
+++ linux.lua
@@ -4,9 +4,9 @@ local wezterm = require 'wezterm'
 local module = {}
 
 function module.apply_config(config)
+	config.enable_wayland = true
 	config.font_size = 11
-	config.window_background_opacity = 0.1
-	config.wayland_window_background_blur = true
+	-- config.wayland_window_background_blur = true
 	config.initial_cols = 90
 	config.initial_rows = 24
 end
