9a1b42b67ed935cb7032d85bafbf5f4fa0111303
Milo Weinberg <git@iapetus11.me>
2026-08-03 08:32:46 -0400

    Increase background blur on MacOS

--- macos.lua
+++ macos.lua
@@ -7,7 +7,7 @@ function module.apply_config(config)
 	config.font_size = 12.5
 	config.initial_cols = 110
 	config.initial_rows = 28
-	config.macos_window_background_blur = 10
+	config.macos_window_background_blur = 20
 end
 
 return module
