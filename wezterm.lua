dc682c6740eb8ae13944f2c7d1cfbe01ba142274
Milo Weinberg <git@iapetus11.me>
2026-08-20 21:45:32 -0400

    Switch color scheme to match Neovim

--- wezterm.lua
+++ wezterm.lua
@@ -6,7 +6,8 @@ local keybinds = require 'keybinds'
 
 local config = wezterm.config_builder()
 
-theming.apply_color_scheme(config, 'carbonfox')
+-- theming.apply_color_scheme(config, 'carbonfox')
+theming.apply_color_scheme(config, 'Catppuccin Mocha')
 platform.apply_platform_config(config)
 keybinds.apply_config(config)
 
