f2c912765931a067757a5788c1b1073ee9c699cf
Milo Weinberg <git@iapetus11.me>
2026-08-20 21:45:11 -0400

    Adjust linux settings again (switching back to wezterm stable)

--- linux.lua
+++ linux.lua
@@ -1,11 +1,8 @@
 local module = {}
 
 function module.apply_config(config)
-	-- Wayland support is busted: wezterm/wezterm#5332
-	config.enable_wayland = false
-
-	config.window_decorations = 'TITLE|RESIZE'
-	config.window_background_opacity = 0.95
+	config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+	config.window_background_opacity = 0.975
 	config.font_size = 11
 	config.initial_cols = 90
 	config.initial_rows = 24
