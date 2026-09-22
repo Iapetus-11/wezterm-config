64b402f908838e47aa55eb69c0ba55166410dfe8
Milo Weinberg <git@iapetus11.me>
2026-09-21 21:43:14 -0400

    Remove background opacity on Linux

--- linux.lua
+++ linux.lua
@@ -2,7 +2,7 @@ local module = {}
 
 function module.apply_config(config)
 	config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
-	config.window_background_opacity = 0.975
+	config.window_background_opacity = 1
 	config.font_size = 11
 	config.initial_cols = 90
 	config.initial_rows = 24
